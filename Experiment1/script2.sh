6c5eb0732b51acbeea5b3ba58e0f5043b9eaf960
Shubham Saxena <shubham.saxena_cs.ccv19@gla.ac.in>
2022-06-02 22:20:47 +0530

    Create script2.sh

---
+++ Experiment1/script2.sh
@@ -0,0 +1,7 @@
+groups
+ec2-user adm wheel systemd-journal www
+sudo chgrp -R www /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+find /var/www -type f -exec sudo chmod 0664 {} +
+cd /var/www/html
